848c06e4feead638147189c61034101e508119fb
Axel Smeets <murlocbrand@gmail.com>
2019-08-19 19:50:07 +0200

    cache html for 1 hr

--- tuna.go
+++ tuna.go
@@ -1,22 +1,45 @@
 package main
 
 import "net/http"
-import "io"
 import "log"
+import "time"
+import "io/ioutil"
 
-func readHtml(w http.ResponseWriter, r *http.Request) {
+var datastore []byte
+var lastfetch time.Time
+
+func update() error {
+	lastfetch = time.Now()
+
 	resp, err := http.Get("https://www.ica.se/butiker/kvantum/lund/ica-kvantum-malmborgs-tuna-2780/erbjudanden/")
 	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
+		return err
 	}
 
 	defer resp.Body.Close()
-	io.Copy(w, resp.Body)
-	//w.Write(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	datastore = body
+	return nil
+}
+
+func readHtml(w http.ResponseWriter, r *http.Request) {
+	if time.Since(lastfetch) > time.Hour {
+		if err := update(); err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+	}
+
+	w.Write(datastore)
 }
 
 func main() {
+	update()
+
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
 	http.HandleFunc("/html", readHtml)
